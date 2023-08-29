82c64b8b081665d0c122945deb9dbec7b08fac1e
Denis Patsev <patsev.den@mail.ru>
2023-08-29 12:32:13 +0300

    script TargetMotion was changed

--- Assets/EnemyGenerator/Scripts/TargetMotion.cs
+++ Assets/EnemyGenerator/Scripts/TargetMotion.cs
@@ -3,30 +3,30 @@ using UnityEngine;
 public class TargetMotion : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private Transform _startPointPosition;
 
-    private SpriteRenderer _targetSpriteRenderer;
+    private SpriteRenderer _spriteRenderer;
+    private float _distance;
 
     private void Start()
     {
-        _targetSpriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _distance = _startPointPosition.position.x + 5;
     }
 
     private void Update()
     {
-            transform.Translate(Time.deltaTime * _speed, 0, 0);
-    }
+        transform.Translate(Time.deltaTime * _speed, 0, 0);
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        _speed *= -1;
-
-        if (collision.gameObject.TryGetComponent(out StartPoint startPoint))
+        if (transform.position.x >= _distance)
         {
-            _targetSpriteRenderer.flipX = false;
+            _spriteRenderer.flipX = true;
+            _speed *= -1;
         }
-        else if (collision.gameObject.TryGetComponent(out EndPoint endPoint))
+        else if (transform.position.x <= _startPointPosition.position.x) 
         {
-            _targetSpriteRenderer.flipX = true;
+            _spriteRenderer.flipX = false;
+            _speed *= -1;
         }
     }
 }
